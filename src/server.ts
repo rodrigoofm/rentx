eb44082c1e3baa9d8dc98ec4cf085842cce55168
Rodrigo Fernandes <f.rodrigo095@gmail.com>
2022-01-31 09:35:41 -0300

    Criando a classe de categoria

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,11 @@
 import express from "express";
 
+import { categoriesRoutes } from "./routes/categories.routes";
+
 const app = express();
 app.use(express.json());
 
-app.get("/", (request, response) => {
-  response.json({ message: "Server running" });
-});
-
-app.post("/courses", (request, response) => {
-  const { name } = request.body;
-
-  return response.json({ name });
-});
+app.use("/categories", categoriesRoutes);
 
 app.listen(3333, () =>
   console.log("Server is running at http://localhost:3333")
