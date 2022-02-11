21aa1681b605c614cd1732b238526375fd0387d0
Rodrigo Fernandes <f.rodrigo095@gmail.com>
2022-02-11 16:25:49 -0300

    Tsyringe injeção de dependencias

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 
-import { specificationController } from "../modules/cars/useCases/createSpecification";
+import { CreateSpecificationController } from "../modules/cars/useCases/createSpecification/CreateSpecificationController";
 
 const specificationsRoutes = Router();
 
-specificationsRoutes.post("/", (request, response) => {
-  return specificationController.handle(request, response);
-});
+const createSpecificationController = new CreateSpecificationController();
+
+specificationsRoutes.post("/", createSpecificationController.handle);
 
 export { specificationsRoutes };
